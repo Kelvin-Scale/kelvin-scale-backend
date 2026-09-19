ee9b945672d261a991763e94073a4d6a77a4da39
avinashponnam <avinashponnam2004@gmail.com>
2026-09-19 15:04:52 +0530

    Kelvin Scale backend

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+
+const flavours = require("./data/flavours");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// GET ALL FLAVOURS
+app.get("/api/flavours", (req, res) => {
+    res.json(flavours);
+});
+
+
+// GET ONLY AVAILABLE FLAVOURS
+app.get("/api/flavours/available", (req, res) => {
+    const availableFlavours = flavours.filter(
+        flavour => flavour.available
+    );
+
+    res.json(availableFlavours);
+});
+
+
+// CHANGE AVAILABILITY
+app.put("/api/flavours/:id", (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const flavour = flavours.find(
+        item => item.id === id
+    );
+
+    if (!flavour) {
+        return res.status(404).json({
+            message: "Flavour not found"
+        });
+    }
+
+    flavour.available = req.body.available;
+
+    res.json({
+        message: "Availability updated",
+        flavour: flavour
+    });
+});
+
+app.use("/admin", express.static("admin"));
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Kelvin Scale Backend running on port ${PORT}`);
+});
